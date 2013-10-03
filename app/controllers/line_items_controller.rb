5600caab205f2e05cece3b57fec2657fd3147bac
phamash <ashleypham19@gmail.com>
2013-10-03 00:40:15 -0400

    gittest

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,6 +1,5 @@
 class LineItemsController < ApplicationController
-  before_action :set_line_item, only: [:show, :edit, :update, :destroy]
-
+ before_filter :set_cart, only: [:create]
   # GET /line_items
   # GET /line_items.json
   def index
@@ -24,12 +23,16 @@ class LineItemsController < ApplicationController
   # POST /line_items
   # POST /line_items.json
   def create
-    @line_item = LineItem.new(line_item_params)
+@cart = set_cart
+     product = Product.find(params[:product_id])
 
+    @Line_item = @cart.line_items.build
+
+    @line_item.product = product
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item, notice: 'Line item was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @line_item }
+        format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
+        format.json { render json: @line_item, status: :created, location: @line_item }
       else
         format.html { render action: 'new' }
         format.json { render json: @line_item.errors, status: :unprocessable_entity }
